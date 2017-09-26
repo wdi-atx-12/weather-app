1b45045c9dddcc4bd7f3a02710879574c1cae64f
Peter Kong <bansuki@gmail.com>
2017-09-26 16:57:12 -0500

    got data pulling and displaying to page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 /* Javascript goes here! */
+$(function () {
+  $('.getWeatherData').submit(requestWeatherData);
+});
+
+function requestWeatherData(e) {
+  e.preventDefault();
+  $.ajax({
+    method: 'GET',
+    dataType: 'json',
+    data: getDataSerialized(),
+    url: 'https://api.openweathermap.org/data/2.5/weather',
+    success: onSuccess
+  });
+}
+
+function onSuccess(response) {
+  $('.temperature').text(kelvinToFahrenheit(response.main.temp) + '° F');
+  $('.humidity').text(response.main.humidity + '%');
+  $('.description').text(response.weather.map(w => w.description).join(' '));
+}
+
+function getDataSerialized(obj = {}) {
+  let serialData = $('.getWeatherData').serialize();
+  for(key in obj) {
+    serialData += `&${key}=${obj[key]}`;
+  }
+  return serialData;
+}
+
+function kelvinToFahrenheit(k) {
+  return (parseFloat(k)-273.15) * 9/5 + 32;
+}
