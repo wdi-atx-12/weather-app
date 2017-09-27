a736c04df2747e62c9dd4bad72fd07aa4715911e
Carrington Simecheck <c.simecheck89@yahoo.com>
2017-09-26 21:00:09 -0500

    too hard

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-/* Javascript goes here! */
+$(document).ready(function(){
+
+var myCity = $('.weather-city').val().trim();;
+$('form').submit('click', function(event) {
+
+    event.preventDefault();
+
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=${myCity}&&appid=373b383635979fcc983487687a41a48a',
+      dataType: 'json',
+      success: onSuccess,
+      error: onError
+    })
+  });
+
+  function onSuccess(data) {
+    console.log("Success");
+    console.log(data);
+    var temp = (data.main.temp);
+    var fahren = (9/5) * (temp - 273) + 32;
+    var humid = (data.main.humidity);
+    $('.temperature').text(Math.round(fahren) + " degrees Fahrenheit");
+    $('.humidity').text(humid + '%');
+  };
+
+  function onError () {
+    console.log("Error: Please input a valid city name.");
+  };
+});
