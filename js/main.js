d52d14b4d296f7c9c2e43e25132a80d6597727bf
TJ Loughry <tjloughry@gmail.com>
2017-09-26 20:24:24 -0500

    needs search, flag, css

--- js/main.js
+++ js/main.js
@@ -14,32 +14,57 @@ function requestWeatherData(){
   console.log('requesting data');
   //this should request live weather data from the API
   //eventually based on the user types
+  $("form").submit(function(event){
+  event.preventDefault();
+  $(".temperature").text('');
+  $(".humidity").text('');
+  $(".results-city").text('');
+
+  console.log('huh?')
+
 
 
   $.ajax({
     method: 'GET',
-    url: 'http://api.openweathermap.org/data/2.5/weather?q=Austin&appid=50f94f818455a348c7481a5574d8700d',
-    data: '',
+    // url: 'http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=50f94f818455a348c7481a5574d8700d',
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=paris&appid=50f94f818455a348c7481a5574d8700d',
+
+    // data: $("form").serialize(),
     // type: 'default GET (Other values: POST)',
     dataType: 'json',
     // data: {param1: 'value1'},
     success: onSuccess,
     error: onError
+
+  })
+   console.log( $('form').serialize() );
   });
 }
 
 function onSuccess(data){
   //TODO: code me
+
+  var kelvin = data.main.temp;
+  var fahrenheit =  ((9/5)*(kelvin - 273) + 32);
+  $(".temperature").append(fahrenheit.toFixed(2) + '°F');
+  $(".humidity").append(data.main.humidity+"%");
+  var weatherIcon = data.weather[0].icon;
+  var countryFlag = data.sys.country
+  $(".results-city").append(data.name + '<img src=http://openweathermap.org/img/w/' + weatherIcon + '.png>');
+  $(".results-city").append(data.sys.country +'<img src= url(../../flags/4x3/' + countryFlag + '.svg)>');
+
   console.log('Request Successful');
   console.dir(data);
-  console.dir(data);
-  console.dir(data.weather[0]);
-  console.dir(data.weather[0].description);
+  console.dir(data.main);
+  console.dir(data.main.temp);
+  console.dir(data.main.humidity);
+  console.log(data.sys.country);
 
 
 }
 
-function onError(){
+function onError(data){
   //TODO: code me
+  $(".city-error").css("display", "block");
   console.log('Error Error Error');
 }
