bb8f8fca03b602d399592f97e92a8404c92de6da
TJ Loughry <tjloughry@gmail.com>
2017-09-26 21:40:59 -0500

    just needs css

--- js/main.js
+++ js/main.js
@@ -16,23 +16,25 @@ function requestWeatherData(){
   //eventually based on the user types
   $("form").submit(function(event){
   event.preventDefault();
+
+
   $(".temperature").text('');
   $(".humidity").text('');
   $(".results-city").text('');
-
-  console.log('huh?')
+  $(".results-flag").text('');
+  console.log('huh?');
 
 
 
   $.ajax({
     method: 'GET',
-    // url: 'http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=50f94f818455a348c7481a5574d8700d',
-    url: 'http://api.openweathermap.org/data/2.5/weather?q=paris&appid=50f94f818455a348c7481a5574d8700d',
+    url: 'http://api.openweathermap.org/data/2.5/weather',
+    // url: 'http://api.openweathermap.org/data/2.5/weather?q=austin&appid=50f94f818455a348c7481a5574d8700d',
 
     // data: $("form").serialize(),
     // type: 'default GET (Other values: POST)',
     dataType: 'json',
-    // data: {param1: 'value1'},
+    data: $("form").serialize(),
     success: onSuccess,
     error: onError
 
@@ -43,15 +45,17 @@ function requestWeatherData(){
 
 function onSuccess(data){
   //TODO: code me
-
+  var weatherIcon = data.weather[0].icon;
+  var weatherType = data.weather[0].description;
+  var countryFlag = data.sys.country
   var kelvin = data.main.temp;
   var fahrenheit =  ((9/5)*(kelvin - 273) + 32);
+  $(".results-flag").append('<img src= flag-icon-css-master/flags/4x3/'+countryFlag+'.svg>');
   $(".temperature").append(fahrenheit.toFixed(2) + '°F');
   $(".humidity").append(data.main.humidity+"%");
-  var weatherIcon = data.weather[0].icon;
-  var countryFlag = data.sys.country
+
   $(".results-city").append(data.name + '<img src=http://openweathermap.org/img/w/' + weatherIcon + '.png>');
-  $(".results-city").append(data.sys.country +'<img src= url(../../flags/4x3/' + countryFlag + '.svg)>');
+  $(".description").append(weatherType)
 
   console.log('Request Successful');
   console.dir(data);
