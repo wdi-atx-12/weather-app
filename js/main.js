e2429345a2bd92f0ff910f830296f509ce272aed
Matt <mattray0295@gmail.com>
2017-09-26 21:42:56 -0500

    refactoring

--- js/main.js
+++ js/main.js
@@ -13,56 +13,36 @@ $(function() {
     clearVals();
     e.preventDefault();
     let $getInput = $('.weather-city').val().trim();
-    if ($getInput === ''){
-      $('.city-error').css('display', 'block');
-    }
-    else {
-      $('.city-error').css('display', 'none');
-    }
     let city = $getInput;
     city = city.replace(/ /g, "+");
-
-    function requestWeatherData() {
-      console.log('Requesting Data');
-
-      $.ajax({
-        method: 'GET',
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`,
-        dataType: 'json',
-      })
-      .done((response) => {
-        let values = getWeatherVals(response);
-        printToDOM(values);
-        
-      })
-      .fail(() => {
-        console.log('Your shit broke');
-      });
-    };
-
-    function getWeatherVals(response) {
-      let weatherVals = new Array(3);
-      let temperature = response.main.temp;
-      weatherVals[0] = (9/5 * (temperature - 273) + 32).toFixed(2);
-      weatherVals[1] = response.main.humidity;
-      weatherVals[2] = response.weather[0].main;
-      weatherVals[3] = response.weather[0].icon;
-      weatherVals[4] = response.name;
-      weatherVals[5] = response.sys.country.toLowerCase();
-      return weatherVals;
-    };
-
-    function printToDOM(values) {
-      $temperature.append(`${values[0]} &deg;F`);
-      $humidity.append(`${values[1]} %`);
-      $description.append(`${values[2]}`);
-      $weatherImg.attr('src', `http://openweathermap.org/img/w/${values[3]}.png`);
-      $cityHeading.append(`${values[4]}`);
-      $flagImg.attr('src', `flags/4x3/${values[5]}.svg`);
-    };
-    // flag-icon flag-icon-gr
-    requestWeatherData();
+    requestWeatherData(city);
   });
+
+
+  function requestWeatherData(city = 'Austin') {
+    $.ajax({
+      method: 'GET',
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`,
+      dataType: 'json',
+    })
+    .done((response) => {
+      getWeatherVals(response);
+    })
+    .fail(() => {
+      $('.city-error').css('display', 'block');
+    });
+  };
+
+  function getWeatherVals(response) {
+    $temperature.text(response.main.temp);
+    $humidity.text(response.main.humidity);
+    $description.text(response.weather[0].main);
+    let imgCode = response.weather[0].icon;
+    $weatherImg.attr('src', `http://openweathermap.org/img/w/${imgCode}.png`);
+    $cityHeading.text(response.name);
+    let countryCode = response.sys.country.toLowerCase();
+    $flagImg.attr('src', `flags/4x3/${countryCode}.svg`);
+  };
 
   function clearVals() {
     let varArry = [$humidity, $temperature, $description, $weatherImg, $cityHeading, $flagImg];
@@ -73,7 +53,6 @@ $(function() {
         return this.nodeType === 3;
       }).remove();
     });
-
-
   };
+  
 });
